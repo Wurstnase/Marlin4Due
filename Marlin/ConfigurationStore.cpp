5eedc1fa78e7fd071a201c14a890d46a0f409f55
Wurstnase <wurstnase.reprap@gmail.com>
2015-03-09 22:00:17 +0100

    little delay keeps...
    the error away. :D

--- Marlin/ConfigurationStore.cpp
+++ Marlin/ConfigurationStore.cpp
@@ -72,6 +72,7 @@ void _EEPROM_writeData(int &pos, uint8_t* value, uint8_t size) {
   uint8_t c;
   while(size--) {
     eeprom_write_byte((unsigned char*)pos, *value);
+    _delay_ms(1);
     c = eeprom_read_byte((unsigned char*)pos);
     if (c != *value) {
       SERIAL_ECHO_START;
