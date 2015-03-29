2c8aef5b5584a4f4e067a1dcc25bf149cd3116ec
Wurstnase <tonnhofer@gmail.com>
2015-03-29 16:31:08 +0200

    No Hardware Serial!

--- Marlin/Conditionals.h
+++ Marlin/Conditionals.h
@@ -167,10 +167,6 @@
 
   #define CONDITIONALS_H
 
-  #ifndef AT90USB
-    #define HardwareSerial_h // trick to disable the standard HWserial
-  #endif
-
   #if (ARDUINO >= 100)
     #include "Arduino.h"
   #else
