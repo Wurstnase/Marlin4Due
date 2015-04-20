50dec7877ae3eb7fba27489806e2683f691ec5f6
Wurstnase <wurstnase.reprap@gmail.com>
2015-04-20 13:45:53 +0200

    add ramps4due

--- Marlin/boards.h
+++ Marlin/boards.h
@@ -10,6 +10,7 @@
 #define BOARD_RADDS			402  // RADDS
 #define BOARD_RAMPS_FD_V1	403  // RAMPS-FD V1
 #define BOARD_RAMPS_FD_V2	404  // RAMPS-FD V2
+#define BOARD_RAMPS4DUE     433  // RAMPS4DUE with AndrewBCN's RAMPS mods (http://forums.reprap.org/read.php?219,479626,page=1)
 
 #define MB(board) (MOTHERBOARD==BOARD_##board)
 
