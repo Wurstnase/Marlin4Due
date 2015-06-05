55cc6d5eef22fc70449033a6c5aeaf6425a8b630
Wurstnase <wurstnase.reprap@gmail.com>
2015-06-05 08:28:27 +0200

    add missing define for high speed stepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -572,6 +572,7 @@ const bool Z_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the logic
 #define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,4000,500}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {300, 300, 5, 25}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
+#define ENABLE_HIGH_SPEED_STEPPING    false  // if your printer runs below 1/32 micro stepping, you don't need to enable it.
 
 #define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // E acceleration in mm/s^2 for retracts
