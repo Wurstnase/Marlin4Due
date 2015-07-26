0ce51c7eb438b5b48680b69188bd2e27354a20a3
wurstnase <wurstnase.reprap@gmail.com>
2015-07-26 09:54:23 +0200

    -eclipse-

--- Marlin/.ino.cpp
+++ Marlin/.ino.cpp
@@ -1,7 +1,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2015-06-10 22:58:44
+//This file has been generated on 2015-07-26 09:50:53
 
 #include "Arduino.h"
 #include "Configuration.h"
