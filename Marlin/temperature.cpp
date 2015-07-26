cec8bb85369b555eb019c1a52d41378e9662b47f
wurstnase <wurstnase.reprap@gmail.com>
2015-07-26 19:20:16 +0200

    more ADC stuff freerun missing in temp.cpp

--- Marlin/temperature.cpp
+++ Marlin/temperature.cpp
@@ -887,8 +887,30 @@ void tp_init() {
 
   // Set analog inputs
   
-  // nothing todo for ARM
-  
+  // Setup channels
+
+    ADC->ADC_MR |= ADC_MR_FREERUN_ON |
+    		  	  	 ADC_MR_LOWRES_BITS_12;
+
+    #define START_TEMP(temp_id) startAdcConversion(pinToAdcChannel(TEMP_## temp_id ##_PIN))
+    #define START_BED_TEMP() startAdcConversion(pinToAdcChannel(TEMP_BED_PIN))
+
+    #if HAS_TEMP_0
+      START_TEMP(0);
+    #endif
+    #if HAS_TEMP_BED
+      START_BED_TEMP();
+    #endif
+    #if HAS_TEMP_1
+      START_TEMP(1)
+    #endif
+    #if HAS_TEMP_2
+      START_TEMP(2)
+    #endif
+    #if HAS_TEMP_3
+      START_TEMP(3)
+    #endif
+
   // Use timer0 for temperature measurement
   // Interleave temperature interrupt with millies interrupt
   
@@ -1374,16 +1396,13 @@ HAL_TEMP_TIMER_ISR {
     } // (pwm_count % 64) == 0
   
   #endif // SLOW_PWM_HEATERS
-
-  #define START_TEMP(temp_id) startAdcConversion(pinToAdcChannel(TEMP_## temp_id ##_PIN))
-  #define START_BED_TEMP() startAdcConversion(pinToAdcChannel(TEMP_BED_PIN))
   
-  #define READ_TEMP(temp_id) temp_read = getAdcReading(pinToAdcChannel(TEMP_## temp_id ##_PIN)); \
+  #define READ_TEMP(temp_id) temp_read = getAdcFreerun(pinToAdcChannel(TEMP_## temp_id ##_PIN)); \
     raw_temp_value[temp_id] += temp_read; \
     max_temp[temp_id] = max(max_temp[temp_id], temp_read); \
     min_temp[temp_id] = min(min_temp[temp_id], temp_read)
     
-  #define READ_BED_TEMP(temp_id) temp_read = getAdcReading(pinToAdcChannel(TEMP_BED_PIN)); \
+  #define READ_BED_TEMP(temp_id) temp_read = getAdcFreerun(pinToAdcChannel(TEMP_BED_PIN)); \
     raw_temp_bed_value += temp_read; \
     max_temp[temp_id] = max(max_temp[temp_id], temp_read); \
     min_temp[temp_id] = min(min_temp[temp_id], temp_read)
@@ -1392,7 +1411,7 @@ HAL_TEMP_TIMER_ISR {
   switch(temp_state) {
     case PrepareTemp_0:
       #if HAS_TEMP_0
-        START_TEMP(0);
+        //START_TEMP(0);
       #endif
       lcd_buttons_update();
       temp_state = MeasureTemp_0;
@@ -1406,7 +1425,7 @@ HAL_TEMP_TIMER_ISR {
 
     case PrepareTemp_BED:
       #if HAS_TEMP_BED
-        START_BED_TEMP();
+        //START_BED_TEMP();
       #endif
       lcd_buttons_update();
       temp_state = MeasureTemp_BED;
@@ -1481,7 +1500,6 @@ HAL_TEMP_TIMER_ISR {
 
     case StartupDelay:
       temp_state = PrepareTemp_0;
-      analogReadResolution(12); //  ADC need some rework
       break;
 
     // default:
