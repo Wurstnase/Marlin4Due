57b3cc2c13bd42204243ef4648916fe5f1ee58b7
wurstnase <wurstnase.reprap@gmail.com>
2015-07-28 21:47:47 +0200

    faster sparkLCD

--- Marlin/ultralcd_st7920_u8glib_rrd.h
+++ Marlin/ultralcd_st7920_u8glib_rrd.h
@@ -27,8 +27,10 @@ static void ST7920_SWSPI_SND_8BIT(uint8_t val)
   for( i=0; i<8; i++ )
   {
     WRITE(ST7920_CLK_PIN,0);
+    delayMicroseconds(2);
     WRITE(ST7920_DAT_PIN,val&0x80); 
     val<<=1;
+    delayMicroseconds(2);
     WRITE(ST7920_CLK_PIN,1);
   }
 }
