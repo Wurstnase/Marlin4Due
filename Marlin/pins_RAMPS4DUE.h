02b241437659ad21b1256d802e5e07cf2a7199c8
wurstnase <wurstnase.reprap@gmail.com>
2015-08-03 22:27:15 +0200

    Define servo pins just like other pins

--- Marlin/pins_RAMPS4DUE.h
+++ Marlin/pins_RAMPS4DUE.h
@@ -4,6 +4,11 @@
 * for RAMPS4DUE (http://forums.reprap.org/read.php?219,479626,page=1)
 ****************************************************************************************/
 
+#define SERVO0_PIN         11
+#define SERVO1_PIN          6
+#define SERVO2_PIN          5
+#define SERVO3_PIN          4
+
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
@@ -107,19 +112,6 @@
 #endif
 
 #define TEMP_BED_PIN       10   // ANALOG NUMBERING
-
-#if HAS_SERVOS
-  #define SERVO0_PIN         11
-  #if NUM_SERVOS > 1
-    #define SERVO1_PIN        6
-    #if NUM_SERVOS > 2
-      #define SERVO2_PIN      5
-      #if NUM_SERVOS > 3
-        #define SERVO3_PIN    4
-      #endif
-    #endif
-  #endif
-#endif
 
 #if ENABLED(Z_PROBE_SLED)
   #define SLED_PIN         -1
